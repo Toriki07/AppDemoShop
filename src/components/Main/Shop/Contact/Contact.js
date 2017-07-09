71c907d1a7a2f37cbe109028830cd73d64952027
Le Hoang Phuong <sigalaxy07@gmail.com>
2017-07-10 00:55:14 +0700

    react native maps

--- src/components/Main/Shop/Contact/Contact.js
+++ src/components/Main/Shop/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 import map from '../../../../media/appIcon/map.png';
 
 import phoneIcon from '../../../../media/appIcon/phone.png';
@@ -16,67 +17,80 @@ class Contact extends Component {
         return (
             <View style={wrapper}>
                 <View style={mapContainer}>
-                    <Image 
-                        style={{ flex: 1, alignSelf: 'stretch', width: undefined }} source={map} 
-                    />
+                    <MapView
+                        style={{ width: width - 20, height: 250 }}
+                        initialRegion={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    >
+                        <MapView.Marker
+                            coordinate={{latitude: 37.78825,
+                            longitude: -122.4324,}}
+                            title="Phuong Le"
+                            description="Teat thu map view"
+                        />
+                        </MapView>
                 </View>
-                <View style={infoContainer}>
-                    <View style={rowInfoContainer}>
-                        <Image source={locationIcon} style={imageStyle} />
-                        <Text style={infoText}>90 Le Thi Rieng/ Ben Thanh Dist</Text>
-                    </View>
-                    <View style={rowInfoContainer}>
-                        <Image source={phoneIcon} style={imageStyle} />
-                        <Text style={infoText}>(+84) 01694472176</Text>
-                    </View>
-                    <View style={rowInfoContainer}>
-                        <Image source={mailIcon} style={imageStyle} />
-                        <Text style={infoText}>khoaphamtraining@gmail.com</Text>
-                    </View>
-                    <View style={[rowInfoContainer, { borderBottomWidth: 0 }]}>
-                        <Image source={messageIcon} style={imageStyle} />
-                        <Text style={infoText}>(+84) 09877067707</Text>
+                    <View style={infoContainer}>
+                        <View style={rowInfoContainer}>
+                            <Image source={locationIcon} style={imageStyle} />
+                            <Text style={infoText}>90 Le Thi Rieng/ Ben Thanh Dist</Text>
+                        </View>
+                        <View style={rowInfoContainer}>
+                            <Image source={phoneIcon} style={imageStyle} />
+                            <Text style={infoText}>(+84) 01694472176</Text>
+                        </View>
+                        <View style={rowInfoContainer}>
+                            <Image source={mailIcon} style={imageStyle} />
+                            <Text style={infoText}>khoaphamtraining@gmail.com</Text>
+                        </View>
+                        <View style={[rowInfoContainer, { borderBottomWidth: 0 }]}>
+                            <Image source={messageIcon} style={imageStyle} />
+                            <Text style={infoText}>(+84) 09877067707</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-        );
+                );
     }
 }
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-    wrapper: { flex: 1, backgroundColor: '#F6F6F6' },
+                    wrapper: {flex: 1, backgroundColor: '#F6F6F6' },
     mapStyle: {
-        width: width - 40,
+                    width: width - 40,
         height: 230,
         alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'center'
     },
     mapContainer: {
-        justifyContent: 'center',
+                    justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
         backgroundColor: '#FFFFFF',
         margin: 10,
         borderRadius: 2,
         shadowColor: '#3B5458',
-        shadowOffset: { width: 0, height: 3 },
+        shadowOffset: {width: 0, height: 3 },
         shadowOpacity: 0.2
     },
     infoContainer: {
-        padding: 10,
+                    padding: 10,
         flex: 1,
         backgroundColor: '#FFF',
         margin: 10,
         marginTop: 0,
         borderRadius: 2,
         shadowColor: '#3B5458',
-        shadowOffset: { width: 0, height: 3 },
+        shadowOffset: {width: 0, height: 3 },
         shadowOpacity: 0.2
     },
     rowInfoContainer: {
-        flex: 1,
+                    flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
@@ -84,11 +98,11 @@ const styles = StyleSheet.create({
         borderColor: '#D6D6D6'
     },
     imageStyle: {
-        width: 30,
+                    width: 30,
         height: 30
     },
     infoText: {
-        fontFamily: 'Avenir',
+                    fontFamily: 'Avenir',
         color: '#AE005E',
         fontWeight: '500'
     }
