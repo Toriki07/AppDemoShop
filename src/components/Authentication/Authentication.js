bae461d87766081faa44bd09dca0f5e702b2cb81
Le Hoang Phuong <sigalaxy07@gmail.com>
2017-08-02 16:21:35 +0700

    Add UI  for SignIn and SignUp

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -9,21 +9,39 @@ import {
 } from 'react-native';
 
 import register from '../../api/register';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 import icBack from '../../media/appIcon/back_white.png';
 import icLogo from '../../media/appIcon/ic_logo.png';
 
 export default class Authentication extends Component {
+  constructor(props) {
+        super(props);
+        this.state = { isSignIn: true };
+    }
 
-  componentDidMount(){
-    register('lrphuong', 'Phuong le', '123')
-    .then(res => console.log(res));
-  }
+    gotoSignIn() {
+        this.setState({ isSignIn: true });
+    }
+
+    signIn() {
+        this.setState({ isSignIn: true });
+    }
+
+    signUp() {
+        this.setState({ isSignIn: false });
+    }
   render() {
     const {
       row1, iconStyle, titleStyle,
       container, controlStyle, signInStyle,
-      signUpStyle, inputStyle, bigButton } = styles;
+      signUpStyle, inputStyle, bigButton,
+      activeStyle, inactiveStyle
+  } = styles;
+
+      const { isSignIn } = this.state;
+        const mainJSX = isSignIn ? <SignIn/> : <SignUp />;
     return (
       <View style={container}>
         <View style={row1}>
@@ -36,20 +54,14 @@ export default class Authentication extends Component {
           <Text style={titleStyle}> LifePlus Demo </Text>
           <Image source={icLogo} style={iconStyle} />
         </View>
-        <View>
-          <TextInput style={inputStyle} placeholder="Enter your email" />
-          <TextInput style={inputStyle} placeholder="Enter your email" />
-          <TouchableOpacity style={bigButton} >
-            <Text>SIGN IN NOW</Text>
-             </TouchableOpacity>
-        </View>
-        <View style={controlStyle}>
-          <TouchableOpacity style={signInStyle}>
-            <Text >SIGN IN</Text>
-          </TouchableOpacity>
 
-          <TouchableOpacity style={signUpStyle}>
-            <Text>SIGN UP</Text>
+        {mainJSX}
+        <View style={controlStyle}>
+          <TouchableOpacity style={signInStyle} onPress={this.signIn.bind(this)}>
+            <Text style={isSignIn ? activeStyle : inactiveStyle}>SIGN IN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={signUpStyle} onPress={this.signUp.bind(this)}>
+            <Text style={!isSignIn ? activeStyle : inactiveStyle}>SIGN UP</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -58,48 +70,40 @@ export default class Authentication extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: '#2EFEF7',
-    padding: 20,
-    justifyContent: 'space-between',
-  },
-  row1: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
-  titleStyle: { color: '#FFF', fontFamily: 'Avenir', fontSize: 30 },
-  iconStyle: { width: 30, height: 30 },
-  controlStyle: {
-    flexDirection: 'row',
-    width: 300,
-  },
-  signInStyle: {
-    backgroundColor: '#FFF',
-    flex: 1,
-    alignItems: 'center',
-    paddingVertical: 15,
-    borderTopLeftRadius: 20,
-    borderBottomLeftRadius: 20,
-    marginRight: 1,
-  },
-  signUpStyle: {
-    backgroundColor: '#FFF',
-    flex: 1,
-    alignItems: 'center',
-    paddingVertical: 20,
-    borderTopRightRadius: 15,
-    borderBottomRightRadius: 20,
-    marginLeft: 1,
-  },
-  inputStyle: {
-    height: 50,
-    backgroundColor: '#FFF',
-    marginBottom: 10,
-    borderRadius: 20,
-    paddingLeft: 30,
-  },
-  bigButton: {
-    height: 50,
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+        flex: 1,
+        backgroundColor: '#3EBA77',
+        padding: 20,
+        justifyContent: 'space-between'
+    },
+    row1: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+    titleStyle: { color: '#FFF', fontFamily: 'Avenir', fontSize: 30 },
+    iconStyle: { width: 30, height: 30 },
+    controlStyle: {
+        flexDirection: 'row',
+        alignSelf: 'stretch'
+    },
+    inactiveStyle: {
+        color: '#D7D7D7'
+    },
+    activeStyle: {
+        color: '#3EBA77'
+    },
+    signInStyle: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingVertical: 15,
+        flex: 1,
+        borderBottomLeftRadius: 20,
+        borderTopLeftRadius: 20,
+        marginRight: 1
+    },
+    signUpStyle: {
+        backgroundColor: '#fff',
+        paddingVertical: 15,
+        alignItems: 'center',
+        flex: 1,
+        marginLeft: 1,
+        borderBottomRightRadius: 20,
+        borderTopRightRadius: 20
+    },
 }); 
